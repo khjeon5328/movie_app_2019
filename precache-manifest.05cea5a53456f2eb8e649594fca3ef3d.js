8a70910a2de296dc8eaf54ba1a8edfd6704a8170
khjeon5 <khjeon5328@naver.com>
2021-10-10 14:43:11 +0900

    Updates

---
+++ precache-manifest.05cea5a53456f2eb8e649594fca3ef3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce6d18c8c2e24cf19de8a706460be6e",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "1f277c5b0652cea50582",
+    "url": "/movie_app_2019/static/css/main.9df05f8c.chunk.css"
+  },
+  {
+    "revision": "915619ba076d60d2e30c",
+    "url": "/movie_app_2019/static/js/2.187c4f4e.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/movie_app_2019/static/js/2.187c4f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f277c5b0652cea50582",
+    "url": "/movie_app_2019/static/js/main.31abe7f2.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
